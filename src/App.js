150327eaeb1b4b136b865f8cdac56ee75e23669b
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2024-03-11 16:12:59 +0530

    Enable Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import { Navbar} from './Components/Navbar';
 import {Textis } from './Components/Textis';
+import React,{useState} from 'react';
 
 function App() {
+  const [mode, setmode] = useState('light');
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.background='#042743'
+    }
+    else{
+      setmode('light');
+      document.body.style.background='white'
+
+    }
+  }
   return (
     <>
-    <Navbar title='TextUtils' about='about text'/>    
+    <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>    
     <div className='container'>
-    <Textis heading='Enter Text'/>
+    <Textis heading='Enter Text' mode={mode}/>
     </div>
     </>
   );
--- src/Components/Textis.js
+++ src/Components/Textis.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+ import React,{ useState } from 'react'
 
 export const Textis=(props)=> {
 const upperClick=()=>{
@@ -25,24 +25,24 @@ const handleChange=(event)=>{
   
   return (
     <>
-    <div>
+    <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
     <h2>{props.heading}</h2>  
 <div className="mb-4">
 <button className='btn btn-danger mb-3'onClick={clearText}>Clear Text</button>
-  <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleChange} rows="8" placeholder='Enter Text'/>
+  <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleChange} style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='white'?'dark':'white'}} rows="8" placeholder='Enter Text'/>
 </div>
 <button className='btn btn-primary mx-2' onClick={upperClick} >Convert to Uppercase</button>
 <button className='btn btn-primary'onClick={lowClick}>Convert to lowercase</button>
 
 </div>
-  <div className='container my-3'>
+  <div className='container my-3' style={{color:props.mode==='light'?'black':'white'}} >
 <h2>Your text summary</h2>
 <p>{text.split(" ").length} words and {text.length} characters</p>
 <p>{0.008*text.split(" ").length} minutes read
 </p>
 <h2>Preview
 </h2>
-<p>{text}</p>
+<p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
   </div>
   </>
   )
