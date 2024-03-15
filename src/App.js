43429eb5e7ec1b013db6c13bf3be2d964cc4661d
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2024-03-15 12:50:23 +0530

    Added Alert

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import { Navbar} from './Components/Navbar';
-import {Textis } from './Components/Textis';
-import React,{useState} from 'react';
-
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+import Alert from "./Components/Alert";
+import { Textis } from "./Components/Textis";
+import React, { useState } from "react";
 function App() {
-  const [mode, setmode] = useState('light');
+  const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
 
-  const toggleMode=()=>{
-    if(mode==='light'){
-      setmode('dark');
-      document.body.style.background='#042743'
+  const ShowAlert = (message, type) => {
+    setalert({
+      message: message,
+      type: type,
+    });
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.background = "#042743";
+      ShowAlert("Dark mode enabled", "success");
+    } else {
+      setmode("light");
+      document.body.style.background = "white";
+      ShowAlert("Light mode enabled", "success");
     }
-    else{
-      setmode('light');
-      document.body.style.background='white'
-
-    }
-  }
+  };
   return (
     <>
-    <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>    
-    <div className='container'>
-    <Textis heading='Enter Text' mode={mode}/>
-    </div>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} ShowAlert={ShowAlert} />
+      <div className="container">
+        <Textis heading="Enter Text" mode={mode} />
+      </div>
     </>
   );
 }
